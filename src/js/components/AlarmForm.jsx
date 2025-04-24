ac689ff10e5f1ed5ca1c173c5049ef7c4aed078b
mouleen <limauropro@gmail.com>
2025-04-24 22:46:04 +0000

    + Un poco mas de UI

--- src/js/components/AlarmForm.jsx
+++ src/js/components/AlarmForm.jsx
@@ -1,6 +1,8 @@
 function AlarmForm({fnAction}){
-    return (
-        <input type="text" onChange={fnAction} id="alarmInput" maxlength="6"/>
+    return (<div className="alarm">
+                <i className="fa-solid fa-bell"></i>
+                <input type="text" onChange={fnAction} id="alarmInput" maxlength="6" placeholder="Segundos para Alarma" className="rounded-4"/>
+            </div>
         )
     }
     export default AlarmForm;
